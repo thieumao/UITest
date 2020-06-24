7c70a948722ed9690be14127018671e61dad48cf
Thieu Nguyen <thieu@bluegrassdigital.com>
2020-06-24 13:52:00 +0700

    Click Avatar

--- tests/specs/mao.spec.js
+++ tests/specs/mao.spec.js
@@ -7,8 +7,8 @@ describe('WebdriverIO and Appium, when interacting with a login form,', () => {
 
     it('should be able click successfully', () => {
         console.log('da vao');
-        // const xpath = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.LinearLayout[1]';
-        // $(xpath).click();
-        // $('~Home').waitForDisplayed(1000);
+        const xpath = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.widget.LinearLayout/androidx.viewpager.widget.ViewPager/android.widget.RelativeLayout/android.widget.LinearLayout/androidx.viewpager.widget.ViewPager/android.view.ViewGroup/android.widget.RelativeLayout/android.widget.RelativeLayout/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup/android.widget.RelativeLayout/android.widget.ImageView';
+        $(xpath).click();
+        $('~Home').waitForDisplayed(1000);
     });
 });

628cdf415dd5542556ff5e56b6f5d0046cde64cf
Thieu Nguyen <thieumao@gmail.com>
2020-06-28 22:56:54 +0700

    Send OK

--- tests/specs/mao.spec.js
+++ tests/specs/mao.spec.js
@@ -6,70 +6,67 @@ describe('GAPO', () => {
     });
 
     it('Chat', () => {
-        console.log('da vao');
-        // const xpath = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.widget.LinearLayout/androidx.viewpager.widget.ViewPager/android.widget.RelativeLayout/android.widget.LinearLayout/androidx.viewpager.widget.ViewPager/android.view.ViewGroup/android.widget.RelativeLayout/android.widget.RelativeLayout/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup/android.widget.RelativeLayout/android.widget.ImageView';
-        // $(xpath).click();
         const chatXpath = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout[3]/android.widget.ImageView';
-        $(chatXpath).click();
         const firstUserXpath = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.widget.LinearLayout/androidx.viewpager.widget.ViewPager/android.view.ViewGroup/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout[1]/android.view.ViewGroup';
-        $(firstUserXpath).click();
         const firstChatField = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.widget.LinearLayout/android.widget.LinearLayout[2]/android.widget.LinearLayout/android.widget.EditText';
-        $(firstChatField).click();
-        $(firstChatField).setValue('Hihi 1');
-
         const sendButtonXpath = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.widget.LinearLayout/android.widget.ImageView';
-        $(sendButtonXpath).click();
-
-        // $(firstChatField).click();
-        // $(firstChatField).setValue('Hihi 2');
-        // $(sendButtonXpath).click();
-
-        // $(firstChatField).click();
-        // $(firstChatField).setValue('Hihi 3');
-        // $(sendButtonXpath).click();
-
-        // $(firstChatField).click();
-        // $(firstChatField).setValue('Hihi 4');
-        // $(sendButtonXpath).click();
-
-        // $(firstChatField).click();
-        // $(firstChatField).setValue('Hihi 5');
-        // $(sendButtonXpath).click();
-
-        // $(firstChatField).click();
-        // $(firstChatField).setValue('Hihi 6');
-        // $(sendButtonXpath).click();
-
         const backXpath = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.RelativeLayout[1]/android.widget.ImageView[1]';
-        $(backXpath).click();
-
         const from = {
             x: 100,
-            y: 700,
+            y: 650,
         };
         const to = {
             x: 100,
             y: 100,
         };
 
-        driver.touchPerform([{
-            action: 'press',
-            options: from,
-        }, {
-            action: 'wait',
-            options: { ms: 1000 },
-        }, {
-            action: 'moveTo',
-            options: to,
-        }, {
-            action: 'release',
-        }]);
-        driver.pause(1000);
+        $(chatXpath).click();
+        let i = 0;
+        while (i < 1000) {
+            i++;
 
-        $(firstUserXpath).click();
-        $(firstChatField).click();
-        $(firstChatField).setValue('22222 ');
-        $(sendButtonXpath).click();
+            $(firstUserXpath).click();
+
+            $(firstChatField).click();
+            $(firstChatField).setValue('Hihi 1');
+            $(sendButtonXpath).click();
+
+            $(firstChatField).click();
+            $(firstChatField).setValue('Hihi 2');
+            $(sendButtonXpath).click();
+
+            $(firstChatField).click();
+            $(firstChatField).setValue('Hihi 3');
+            $(sendButtonXpath).click();
+
+            $(firstChatField).click();
+            $(firstChatField).setValue('Hihi 4');
+            $(sendButtonXpath).click();
+
+            $(firstChatField).click();
+            $(firstChatField).setValue('Hihi 5');
+            $(sendButtonXpath).click();
+
+            $(firstChatField).click();
+            $(firstChatField).setValue('Hihi 6');
+            $(sendButtonXpath).click();
+
+            $(backXpath).click();
+
+            driver.touchPerform([{
+                action: 'press',
+                options: from,
+            }, {
+                action: 'wait',
+                options: { ms: 1000 },
+            }, {
+                action: 'moveTo',
+                options: to,
+            }, {
+                action: 'release',
+            }]);
+            driver.pause(1000);
+        }
 
         driver.setImplicitTimeout(5000);
     });
